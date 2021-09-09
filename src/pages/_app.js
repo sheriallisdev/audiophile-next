f9f9961bfa53232e187463812869df688720c8c8
Sheri Richardson <shrdson@gmail.com>
2021-09-09 17:52:17 +0200

    Add progress bar on route change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,20 @@ import "@styles/globals.scss";
 
 import { Layout } from "@components/common";
 
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+
+const progress = new ProgressBar({
+  size: 4,
+  color: "#D87D4A",
+  className: "progressBar",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", process.finish);
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
