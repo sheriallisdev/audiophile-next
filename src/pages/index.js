0b9a608286a09bd0963f7ab7768d5870011666f0
Sheri Richardson <shrdson@gmail.com>
2021-10-11 22:07:27 +0200

    Add category links and about section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,49 @@
+import { About, CategoryGrid } from "@components/common";
 import { Hero } from "@components/ui";
 
-export default function Home() {
+import { GraphQLClient } from "graphql-request";
+const graphcms = new GraphQLClient(process.env.GRAPHCMS_API);
+
+const query = `
+  query HomepageQuery{
+    categories {
+      name
+      image {
+        url
+      }
+      slug
+    }
+    info(where: {id: "cktsl7960fynm0b605xp188nl"}) {
+      description
+      image {
+        url
+      }
+      tagLine {
+        raw
+      }
+    }
+  }
+`;
+
+export default function Home({ categories, info }) {
   return (
     <div>
       <main>
         <Hero />
+        <CategoryGrid categories={categories} />
+        <About info={info} />
       </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const { categories, info } = await graphcms.request(query);
+
+  return {
+    props: {
+      categories,
+      info,
+    },
+  };
 }
