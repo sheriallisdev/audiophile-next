8441de13d3558a4860bc8974a981789f15e0ee0f
Sheri Richardson <shericodes@protonmail.com>
2022-03-15 16:28:23 +0100

    Add featured products

--- src/components/home/FeaturedProducts/ProductThree/ProductThree.js
+++ src/components/home/FeaturedProducts/ProductThree/ProductThree.js
@@ -1,11 +1,21 @@
+import { Card, StyledLink } from "@components/ui";
+import styles from "./ProductThree.module.scss";
+
 const ProductThree = () => {
   return (
     <>
-      <div>
-        <h2>YX1 EARPHONES</h2>
-      </div>
-      <div>
-        <h2>YX1 EARPHONES</h2>
+      <div className={styles.wrapper}>
+        <Card className={styles["image-card"]}>
+          <img src="/assets/home/desktop/image-earphones-yx1.jpg" />
+        </Card>
+        <Card className={styles["content-card"]}>
+          <div className={styles.content}>
+            <h2 className={styles.title}>YX1 EARPHONES</h2>
+            <StyledLink href={`/product/zx-7-speaker`} className="outline">
+              See Product
+            </StyledLink>
+          </div>
+        </Card>
       </div>
     </>
   );
