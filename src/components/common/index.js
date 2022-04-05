0bffac65210a940fe52215d26c6606cc0cdd3747
Sheri Richardson <shericodes@protonmail.com>
2022-04-05 21:15:06 +0200

    Add mobile menu

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -5,3 +5,4 @@ export { default as Layout } from "./Layout";
 export { default as CategoryGrid } from "./CategoryGrid";
 export { default as About } from "./About";
 export { default as Footer } from "./Footer";
+export { default as MobileMenu } from "./MobileMenu";
