2794caa498a92d0e35cbd505e0f269efb94a9e3b
Sheri Richardson <shericodes@protonmail.com>
2022-04-05 21:14:27 +0200

    Add backdrop component

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -6,3 +6,4 @@ export { default as Quantity } from "./Quantity";
 export { default as StyledLink } from "./StyledLink";
 export { default as Hero } from "./Hero";
 export { default as Card } from "./Card";
+export { default as Backdrop } from "./Backdrop";
